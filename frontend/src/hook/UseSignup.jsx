c3dea2fb8b50bc4d6ea292f055b6337e4832649b
dripCoder-design <coderdrip@gmail.com>
2024-10-05 23:52:47 +0300

    Update UseSignup.jsx

--- frontend/src/hook/UseSignup.jsx
+++ frontend/src/hook/UseSignup.jsx
@@ -8,7 +8,7 @@ const { dispatch } = useAuthContext()
 const signUp = async(email,password) => {
     setIsLoading(true)
     setError(null)
-    const response = await fetch('http://localhost:3000/api/user/signup/',{
+    const response = await fetch('https://team-i9x4.onrender.com/api/user/signup/',{
         method: 'POST',
         headers: {'Content-type' : 'application/json'},
         body: JSON.stringify({email, password})
