1cd43bfd7347c75c49add668e8fb53d574b2885b
dripCoder-design <coderdrip@gmail.com>
2024-10-05 23:55:39 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const { user } = useAuthContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-     const response = await fetch('http://localhost:3000/api/workouts',{
+     const response = await fetch('https://team-i9x4.onrender.com/api/workouts',{
       headers: {
         'Authorization': `Bearer ${user.token}`
       }
