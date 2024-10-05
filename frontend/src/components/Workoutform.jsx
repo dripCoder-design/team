584bce6fc06bff37f8cedd53f4f586bb81426dd9
dripCoder-design <coderdrip@gmail.com>
2024-10-05 23:54:46 +0300

    Update Workoutform.jsx

--- frontend/src/components/Workoutform.jsx
+++ frontend/src/components/Workoutform.jsx
@@ -21,7 +21,7 @@ const Workoutform =()=> {
 
         const workout = {title, load, reps}
 
-        const response = await fetch('http://localhost:3000/api/workouts',{
+        const response = await fetch('https://team-i9x4.onrender.com/api/workouts',{
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
